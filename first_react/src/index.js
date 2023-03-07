e160741d2db1fd671fed882a6691746a790cb9d1
Konrad <kd.krukowski@gmail.com>
2023-03-07 16:22:32 +0100

    modify 1

--- first_react/src/index.js
+++ first_react/src/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
 import './index.css';
+
+function Navbar(){
+  return(
+    <img src="logo192.png" alt="logo"></img>
+  );
+}
+
+function MainContent()
+{
+  return(
+    <div>
+
+      <h1>Fun facts about React</h1>
+      
+      <h3>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was orginally created by Jordan Walke</li>
+          <li>Has well over 100 start on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </h3>
+
+    </div>
+  );
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
+
 root.render(
   <React.StrictMode>
-    <p> Hello </p>
+
+    <Navbar></Navbar>
+    <MainContent></MainContent>
+
   </React.StrictMode>
 );
