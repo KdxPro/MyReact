828464025f8214230bf1bb560ee73cff9193fcf3
Konrad <kd.krukowski@gmail.com>
2023-03-08 17:37:21 +0100

    next progress

---
+++ first_react/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Header } from './Header';
+import { MainContent } from './MainContent';
+import { Footer } from './Footer';
+
+import "./style.css"
+
+export function App() {
+  return (
+    <div>
+
+      <Header />
+      <MainContent />
+      <Footer />
+
+    </div>
+  );
+}
--- first_react/src/index.js
+++ first_react/src/index.js
@@ -1,63 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
-
-function Navbar(){
-  return(
-    <img src="logo192.png" alt="logo" width="40px"></img>
-  );
-}
-
-function MainContent()
-{
-  return(
-    <div>
-
-      <h1>Fun facts about React</h1>
-      
-      <h3>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was orginally created by Jordan Walke</li>
-          <li>Has well over 100 start on Github</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </h3>
-
-    </div>
-  );
-}
-
-
-function Footer()
-{
-  return(
-    <small>© 2023 Konrad development. All rights reserved</small>
-  );
-}
-
-function Page()
-{
-  return(
-    <div>
-
-      <Navbar></Navbar>
-      <MainContent></MainContent>
-      <Footer></Footer>
-
-    </div>
-  );
-}
-
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render( 
   <React.StrictMode>
 
-    <Page></Page>
+    <App/>
 
   </React.StrictMode>
 );
