529359bf3ffc33ce9c40484a2b045f73209dbc6c
Konrad <kd.krukowski@gmail.com>
2023-03-30 16:09:08 +0200

    add fav icon

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import "./App.css";
 
-import { Header } from "./components/Header"
-import { MainContent } from "./components/MainContent"
+import { Header } from "./components/Header";
+import { MainContent } from "./components/MainContent";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <MainContent/>
+      <Header />
+      <MainContent />
     </div>
   );
 }
--- meme_generator/src/components/MainContent.js
+++ meme_generator/src/components/MainContent.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React from "react";
+
+import favIcon from "../images/fav_icon.png";
+import favIconFull from "../images/favFull_icon.png";
 
 export function MainContent() {
-
   const [randVal, setRandVal] = React.useState(0);
-  
-  function handleOnClick(e)
-  {
-    e.preventDefault(); 
+  const [isFavIcon, setFavIcon] = React.useState(false);
 
+  function handleOnClick(e) {
+    e.preventDefault();
+
+    setFavIcon(false);
     setRandVal(Math.floor(Math.random() * 255));
   }
-  
-  
-  return ( 
+
+  function handleOnClickFavIcon() {
+    setFavIcon((prevFavIcon) => !prevFavIcon);
+  }
+
+  return (
     <main>
-      <form className='main--form'>
-        <input className='main--input' placeholder="Top text"></input>
-        <input className='main--input' placeholder="Bottom text"></input>
-        <button className='main--button' onClick={handleOnClick}>Get a new meme image</button>
+      <form className="main--form">
+        <input className="main--input" placeholder="Top text"></input>
+        <input className="main--input" placeholder="Bottom text"></input>
+        <button className="main--button" onClick={handleOnClick}>
+          Get a new meme image
+        </button>
       </form>
-      <img className='main--image' src={"https://picsum.photos/600/400?random=" + randVal} alt="main_image"></img>
+
+      <div className="main--imageContainer">
+        <img
+          className="imageContainer--image"
+          src={"https://picsum.photos/600/400?random=" + randVal}
+          alt="main_image"
+        ></img>
+
+        <img
+          className="imageContainer--fav_icon"
+          src={isFavIcon ? favIconFull : favIcon}
+          alt="main_fav_icon"
+          onClick={handleOnClickFavIcon}
+        ></img>
+      </div>
     </main>
   );
 }
