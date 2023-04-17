ea52428bb2e8973895a829656a4c9db5beeb6115
Konrad <kd.krukowski@gmail.com>
2023-04-17 13:32:43 +0200

    testing new features

--- drag_en_drop/src/App.js
+++ drag_en_drop/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import React from "react";
 
-import { DragEnDrop } from './components/DragEnDrop';
+import "./App.css";
+
+import { DragEnDrop } from "./components/DragEnDrop";
 
 function App() {
+  const [mPos, setMpos] = React.useState({ x: 0, y: 0 });
+  const handleMouseMove = (event) => {
+    setMpos({ x: event.clientX, y: event.clientY });
+  };
+
   return (
-    <div className="App">
-      <DragEnDrop/>
+    <div className="App" onMouseMove={handleMouseMove}>
+      <DragEnDrop mousePosition={mPos} />
     </div>
   );
 }
--- drag_en_drop/src/components/DragEnDrop.js
+++ drag_en_drop/src/components/DragEnDrop.js
@@ -1,3 +1,60 @@
-export function DragEnDrop() {
-  return <div className="dragendrop"></div>;
+import React from "react";
+
+export function DragEnDrop(props) {
+  let corr = { x: 0, y: 0 };
+  let styles = { left: corr.x, top: corr.y };
+
+  const [lastMousCorr, setlastMousCorr] = React.useState({ x: 0, y: 0 });
+  const [lastShapeCorr, setlastShapeCorr] = React.useState({ x: 0, y: 0 });
+
+  const [isClicked, setIsClicked] = React.useState(false);
+  function changeIsClicked() {
+    setIsClicked((prevIsClicked) => !prevIsClicked);
+
+    setlastMousCorr({ x: props.mousePosition.x, y: props.mousePosition.y });
+    setlastShapeCorr({ x: finalCorr.x, y: finalCorr.y });
+  }
+
+  const [finalCorr, setFinalCorr] = React.useState(corr);
+  function changeFinalCorr(x, y) {
+    setFinalCorr({
+      x: lastShapeCorr.x + x,
+      y: lastShapeCorr.y + y,
+    });
+
+    changeFinalStyles(finalCorr.x, finalCorr.y);
+  }
+
+  const [finalStyles, setFinalStyles] = React.useState(styles);
+  function changeFinalStyles(x, y) {
+    setFinalStyles({ left: x, top: y });
+  }
+
+  // function tmp() {
+  //   if (isClicked)
+  //     changeFinalCorr(
+  //       props.mousePosition.x - lastMousCorr.x,
+  //       props.mousePosition.y - lastMousCorr.y
+  //     );
+  //   console.log(1);
+  // }
+  // console.log(2);
+
+  function tmp() {
+    if (isClicked) {
+      changeFinalCorr(
+        props.mousePosition.x - lastMousCorr.x,
+        props.mousePosition.y - lastMousCorr.y
+      );
+    }
+  }
+
+  return (
+    <div
+      className="dragendrop"
+      style={finalStyles}
+      onClick={changeIsClicked}
+      onMouseMove={tmp}
+    ></div>
+  );
 }
