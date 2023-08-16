01d67fa411ec8c7dc01c0b8d9edece31cf9e6a90
Konrad <kd.krukowski@gmail.com>
2023-08-16 15:57:17 +0200

    add radiobutton

--- meme_generator/src/components/MainContent.js
+++ meme_generator/src/components/MainContent.js
@@ -23,6 +23,7 @@ export function MainContent(props) {
     topText: "",
     bottomText: "",
     isGood: true,
+    employment: "",
   });
   function handleFormChange(event) {
     const { name, value, type, checked } = event.target;
@@ -82,6 +83,39 @@ export function MainContent(props) {
         <label htmlFor="isGood">
           Is this App good? {form.isGood ? ":)" : ":("}
         </label>
+      </div>
+      <div className="main--radioContainer">
+        <legend>Current employment status</legend>
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={form.employment === "unemployed"}
+          onChange={handleFormChange}
+        ></input>
+        <label htmlFor="radioButton">Unemployed</label>
+        <br />
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={form.employment === "part-time"}
+          onChange={handleFormChange}
+        ></input>
+        <label htmlFor="radioButton">Part-time</label>
+        <br />
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={form.employment === "full-time"}
+          onChange={handleFormChange}
+        ></input>
+        <label htmlFor="radioButton">Full-time</label>
+        <br />
       </div>
     </main>
   );
